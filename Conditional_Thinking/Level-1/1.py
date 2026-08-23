42ea4022f9b39ee782f79782c3aeb3f8d79ef558
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-23 17:22:13 +0530

    Create 1.py

---
+++ Conditional_Thinking/Level-1/1.py
@@ -0,0 +1,9 @@
+no = int(input("Enter a number: "))
+
+if no > 0:
+    print(f"{no} is positive number")
+elif no < 0:
+    print(f"{no} is negative numebr")
+else:
+    print(f"{no} is zero number")
+

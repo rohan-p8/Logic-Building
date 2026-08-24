b9d31ae54766965ccce7e5b6f376eddc98d87f53
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-24 23:20:29 +0530

    Take character and check it is vowel or consonant

---
+++ Conditional_Thinking/Level-1/9.py
@@ -0,0 +1,10 @@
+# Take a character and check if it’s a vowel or consonant.
+
+char = input("Enter a character : ")
+
+if char == "a" and char == "e" and char == "i" and char == "o" and char == "u":
+    print(f"{char} is vowel")
+else:
+    print(f"{char} is consonant")
+
+    

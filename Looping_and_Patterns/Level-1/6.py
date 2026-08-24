0bb7df95c8c962c69b47f6eead1f245270a3de89
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-24 23:52:45 +0530

    Print the sum of first n natural numbers.

---
+++ Looping_and_Patterns/Level-1/6.py
@@ -0,0 +1,9 @@
+# Print the sum of first n natural numbers. 
+
+n = int(input("Enter a number: "))
+sum = 0
+
+for i in range(1, n + 1):
+    sum = sum + i
+
+print(f"Sum of first {n} natural no. is: ", sum)

d0b26a9f895d058b3ce5f3cfd58c2f9dc0104d2e
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-25 00:21:42 +0530

    Print the factorial of a given number.

---
+++ Looping_and_Patterns/Level-1/9.py
@@ -0,0 +1,10 @@
+# Print the factorial of a given number.
+
+n = int(input("Enter a number : "))
+fact = 1
+
+for i in range(1, n + 1):
+    fact = fact * i
+
+print(f"Factorial of {n} is : ", fact)
+

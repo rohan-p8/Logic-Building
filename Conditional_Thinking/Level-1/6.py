9dba7767d1ba5bb8ee466cf79c84734cd829752e
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-24 22:48:32 +0530

    Print larger no. from two numbers

---
+++ Conditional_Thinking/Level-1/6.py
@@ -0,0 +1,10 @@
+# Take two numbers and print the larger one.
+
+a, b = map(int, input("Enter two no.").split())
+
+if a > b:
+    print(f"{a} is larger number")
+else:
+    print(f"{b} is larger number")
+
+    

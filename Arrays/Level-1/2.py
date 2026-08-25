0a643e90857dea2193a9a22ab52f71baca6311f5
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-25 22:16:16 +0530

    Find the sum of all elements in an array.

---
+++ Arrays/Level-1/2.py
@@ -0,0 +1,18 @@
+# Find the sum of all elements in an array. 
+
+n = int(input("Enter no. of items: "))
+l1 = []
+
+for i in range(n):
+
+    ele = int(input(f"Enter {i + 1} element: "))
+    l1.append(ele)
+
+print("\nEntered list is: ", l1)
+
+sum = 0
+
+for i in range(n):
+    sum = sum + l1[i]
+
+print("Sum of all elemets are: ", sum)

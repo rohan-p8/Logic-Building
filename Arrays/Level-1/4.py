d63529416825835fbb7ec724bd18e33874ef0412
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-25 22:43:27 +0530

    Find the maximum element in an array

---
+++ Arrays/Level-1/4.py
@@ -0,0 +1,18 @@
+# Find the maximum element in an array
+
+list1 = [25,85,60,34,75,99,15,2]
+
+# print(max(list1))
+
+# newList = sorted(list1)
+# print(newList)
+# print(newList[-1])
+print(list1)
+
+max = list1[0]
+
+for i in range(len(list1)):
+    if list1[i] > max:
+        max = list1[i]
+
+print("Maximum element is: ", max)

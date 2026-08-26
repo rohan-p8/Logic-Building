e4fd29bd0f9a9b76348a221373458c7ce5019825
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-26 23:51:35 +0530

    Take two strings and print them concatenated.

---
+++ Strings/Level-1/7.py
@@ -0,0 +1,8 @@
+# Take two strings and print them concatenated. 
+
+str1 = input("Enter firsr string: ")
+str2 = input("Enter second string: ")
+
+print(str1 + " " + str2)
+print(" ".join([str1, str2]))
+

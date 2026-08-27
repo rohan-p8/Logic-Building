23255bcd82ca4061ed5f5251b630318b59e0fd65
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-27 14:46:45 +0530

    Compare two strings lexicographically (like dictionary order).

---
+++ Strings/Level-1/8.py
@@ -0,0 +1,7 @@
+# Compare two strings lexicographically (like dictionary order).
+
+str1 = "Rohan"
+str2 = "suhani"
+
+print(f"{str1} < {str2} : ", str1 < str2)
+print(f"{str1} > {str2} : ", str1 > str2)

960c5f8ff6b70c86c7a2045b444161f5444f6561
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-27 15:13:16 +0530

    Check whether the string is empty or not.

---
+++ Strings/Level-1/10.py
@@ -0,0 +1,9 @@
+# Check whether the string is empty or not. 
+
+str1 = "asd"
+
+
+if len(str1.strip()) == 0:
+    print("String is empty or contains only whitespaces")
+else:
+    print("String:",str1)

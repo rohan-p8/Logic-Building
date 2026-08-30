d33f8ef8bdb2cdd0519e53f7686f54ffd28688f1
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-30 16:33:48 +0530

    Check voting eligibility for a given age (18+).

---
+++ Conditional_Thinking/Level-2/6.py
@@ -0,0 +1,11 @@
+# Check voting eligibility for a given age (18+).
+
+age = int(input("Enter age to check voting eligibility: "))
+
+if age >= 18:
+    print("Person is eligible for Voting")
+
+else:
+    print("Person is not eligible for Voting")
+
+    

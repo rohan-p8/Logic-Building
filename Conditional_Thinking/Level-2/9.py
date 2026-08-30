211d8d805849cefa08f980a70fe4b90672a87e7e
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-30 17:09:13 +0530

    Take a day number (1–7) and print the corresponding day name

---
+++ Conditional_Thinking/Level-2/9.py
@@ -0,0 +1,30 @@
+# Take a day number (1–7) and print the corresponding day name
+
+day = int(input("Enter a day (1-7): "))
+
+if day < 1 or day > 7:
+    print("Invalid day !!!")
+
+else:
+
+    if day == 1:
+        print("Monday")
+
+    elif(day == 2):
+        print("Tuesday")
+
+    elif(day == 3):
+        print("Tuesday")
+
+    elif(day == 4):
+        print("Tuesday")
+
+    elif(day == 5):
+        print("Tuesday")
+
+    elif(day == 6):
+        print("Tuesday")
+
+    else:
+          print("Sunday")
+

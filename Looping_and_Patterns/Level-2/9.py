cc2292f44461c23dafdea81992a6a816bef33317
rohan-p8 <rohanpatil0449@gmail.com>
2026-08-31 22:48:53 +0530

    Print all prime numbers between 1 and 100.

---
+++ Looping_and_Patterns/Level-2/9.py
@@ -0,0 +1,12 @@
+# Print all prime numbers between 1 and 100.
+
+for i in range(2, 101):
+
+    for j in range(2, i):
+
+        if i % j == 0:
+            break
+    else:
+        print(i, end=" ")
+
+        

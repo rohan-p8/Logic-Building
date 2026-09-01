4c80c9249ed929fad07d3db11820cb5ea854546d
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-01 16:58:44 +0530

    Print Fibonacci series up to n terms.

---
+++ Looping_and_Patterns/Level-2/10.py
@@ -0,0 +1,19 @@
+# Print Fibonacci series up to n terms.
+
+terms = int(input("Enter a number of terms:"))
+
+n1, n2 = 0, 1
+print(n1, end=" ")
+print(n2, end=" ")
+
+count = 0
+
+while count < terms:
+	res = n1 + n2
+	print(res, end=" ")
+	
+	n1 = n2
+	n2 = res
+	count += 1
+
+	

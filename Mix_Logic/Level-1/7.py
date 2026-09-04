599e478f713821439ba63e9b8eeca37590915543
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-04 22:48:10 +0530

    Print all prime numbers between 1 and N.

--- Mix_Logic/Level-1/7.py
+++ Mix_Logic/Level-1/7.py
@@ -1,2 +1,13 @@
 # Print all prime numbers between 1 and N. 
 
+n = int(input("Enter number: "))
+print(f"\nPrime numbers between 1 and {n} are: ")
+
+for num in range(2, n + 1):
+	if num > 1:
+		for i in range(2, num):
+			if num % i == 0:
+				break
+		else:
+			print(num, end=" ")
+

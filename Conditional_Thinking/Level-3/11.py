3b7260bfc47bf78f2829e1a15093a444aa4b4d87
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-06 13:47:59 +0530

    Check whether a number is a perfect square

---
+++ Conditional_Thinking/Level-3/11.py
@@ -0,0 +1,22 @@
+#  Check whether a number is a perfect square (without using the square root function). 
+import math
+
+def isPerfectSquare(n):
+
+	if n <= 0:
+		return False
+
+	else:
+		root = int(math.sqrt(n))
+
+
+	return root * root == n
+
+
+n = int(input("Enter number to check perfect square: "))
+
+if isPerfectSquare(n):
+	print("True")
+else:
+	print("False")
+

f3e79db78f2111c93d19f46f114a28ec37d8237a
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-07 22:56:01 +0530

    Find LCM of two numbers using loops.

--- Looping_and_Patterns/Level-3/5.py
+++ Looping_and_Patterns/Level-3/5.py
@@ -8,4 +8,18 @@ for i in range(min_num, 0, -1):
 		lcm = (n1 * n2) // i
 		print(f"\nLCM of {n1} and {n2} is: {lcm}")
 		break
+	
 
+# Find LCM Without finding HCF first
+
+# n1, n2 = map(int, input("Enter two numbers to find LCM: ").split())
+
+# # Start from the larger number and step by that number
+# step = max(n1, n2)
+# multiple = step
+
+# while True:
+#     if multiple % n1 == 0 and multiple % n2 == 0:
+#         print(f"\nLCM of {n1} and {n2} is: {multiple}")
+#         break
+#     multiple += step

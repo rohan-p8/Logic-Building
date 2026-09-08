44ee2b79417cd52e8f7296c83c8609365c3c37d7
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-08 21:45:36 +0530

    Create a new array containing squares of all numbers.

---
+++ Arrays/Level-3/1.py
@@ -0,0 +1,11 @@
+# Create a new array containing squares of all numbers. 
+
+arr = [2,3,5,12,31,15]
+sqrArr = []
+
+for num in arr:
+	sqr = num * num
+	sqrArr.append(sqr)
+
+print(f"List of squres: \n",sqrArr)
+

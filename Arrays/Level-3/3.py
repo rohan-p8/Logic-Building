1b90ca8b4dd806338483b84b44e0f08886deea69
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-08 22:12:58 +0530

    Replace every negative number with 0

---
+++ Arrays/Level-3/3.py
@@ -0,0 +1,16 @@
+# Replace every negative number with 0
+
+arr = [22, 14, 8, -13, -27, 88, -45]
+print("Given list: ", arr)
+
+newArr = []
+
+for num in arr:
+	if num < 0:
+		newArr.append(0)
+
+	else:
+		newArr.append(num)
+
+print(f"\nNew list: ", newArr)
+

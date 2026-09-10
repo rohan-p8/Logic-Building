7ff359b38771ec8c54917b07355c5a9e74b384d5
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-10 16:41:34 +0530

    Copy one array to another manually.

---
+++ Arrays/Level-3/12.py
@@ -0,0 +1,13 @@
+# Copy one array to another manually.
+
+lst = [18, 14, 8, 13, 27, 88, 45]
+print("Given list: ", lst)
+
+newLst = []
+
+for i in range(len(lst)):
+
+	newLst.append(lst[i])
+
+print("New copy of list: ",newLst)
+

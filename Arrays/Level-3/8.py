ecd90fd8dd01117bffb666876d58d2208c807dec
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-10 13:19:07 +0530

    Rotate an list by one position to the left (Manual shift).

---
+++ Arrays/Level-3/8.py
@@ -0,0 +1,16 @@
+# Rotate an list by one position to the left (Manual shift).
+
+lst = [18, 14, 8, 13, 27, 88, 45]
+print("Given list: ", lst)
+
+if len(lst) > 1:
+
+	first = lst[0]
+
+	for i in range(len(lst) - 1):
+		lst[i] = lst[i + 1]
+
+	lst[-1] = first
+
+print(lst)
+

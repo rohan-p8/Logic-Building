6ca7e6fbbc1fb907b29153679901c9c46eb71fa7
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-17 19:26:57 +0530

    Reverse the order of words in a sentence.

--- Strings/Level-3/3.py
+++ Strings/Level-3/3.py
@@ -1,14 +1,13 @@
 # Reverse the order of words in a sentence. 
 
 str1 = input("Enter sentence to change order of words: ").split()
-# str1 = "Hello rohan".split()
 
 newStr = ""
-revOrder = ""
 
 for i in range(len(str1) -1, -1, -1):
 
 	newStr += str1[i] + " "
 
+newStr = newStr.strip()
 print(f"\nReverse order of words: {newStr}")
 

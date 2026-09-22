caa6a954903cc55aefdad8bde0d33d33d426ea0c
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-22 17:49:16 +0530

    Print an Increasing Triangle of Stars

--- Looping_and_Patterns/Level-4/5.py
+++ Looping_and_Patterns/Level-4/5.py
@@ -2,10 +2,9 @@
 
 n = int(input("Enter n to print increasing triangle of stars: "))
 
-for i in range(n):
+for i in range(1, n + 1):
 
-	for j in range(i + 1):
+	for j in range(i):
 		print("*", end = " ")
 	print()
 
-	

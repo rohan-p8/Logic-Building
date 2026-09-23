7761529d630ea12ed0c5c2b626f5c08e4c68e3a9
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-23 19:17:13 +0530

    Print a Centered Pyramid of Stars.

--- Looping_and_Patterns/Level-4/10.py
+++ Looping_and_Patterns/Level-4/10.py
@@ -1,13 +1,13 @@
-# Print a Centered Pyramid of Stars 
+# Print a Centered Pyramid of  Odd number of stars Stars.
 
 n = int(input("Enter number of rows: "))
 
 for i in range(1, n + 1):
 
 	for j in range(n - i):
-		print(" ", end = "")
+		print(" ", end =" ")
 
-	for k in range(i):
+	for k in range(2 * i - 1):
 		print("*", end = " ")
 
 	print()

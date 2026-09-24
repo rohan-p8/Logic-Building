98010b2d6887e00900978c8b0069bccc8d4089cb
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-24 20:54:48 +0530

    Print Numbers in an Increasing Sequence (1, 12, 123, 1234, 12345)

---
+++ Looping_and_Patterns/Level-4/12.py
@@ -0,0 +1,11 @@
+# Print Numbers in an Increasing Sequence (1, 12, 123, 1234, 12345)
+
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n + 1):
+
+	for j in range(1, i + 1):
+		print(j, end ="")
+
+	print()
+
